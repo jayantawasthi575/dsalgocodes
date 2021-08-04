53febf7683e897f68109d4d5e37e757d38b05b54
Jayant <jayantawasthi5@gmail.com>
2021-08-04 23:49:23 +0530

    Convert array into Zig-Zag fashion

---
+++ ds11.cpp
@@ -0,0 +1,21 @@
+class Solution{
+public:	
+	// Program for zig-zag conversion of array
+	void zigZag(int arr[], int n) {
+	    for(int i=0;i<n-1;i++)
+	    {
+	        if(i%2==0 && arr[i]>arr[i+1])
+	        {
+	            int temp=arr[i];
+	            arr[i]=arr[i+1];
+	            arr[i+1]=temp;
+	        }
+	        else if(i%2!=0 && arr[i]<arr[i+1])
+            {
+            int temp=arr[i];
+            arr[i]=arr[i+1];
+            arr[i+1]=temp;  
+            }
+	    }
+	}
+};
