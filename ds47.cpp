53599650600ca33e94e4dc8cbbc32a4cde5eda1b
Jayant <jayantawasthi5@gmail.com>
2021-08-10 23:36:30 +0530

    Element with left side smaller and right side greater

---
+++ ds47.cpp
@@ -0,0 +1,20 @@
+int findElement(int arr[], int n) {
+    int left[n],right[n];
+   
+    left[0]=INT_MIN;
+    right[n-1]=INT_MAX;
+   
+    for(int i=0;i<n-1;i++)
+       left[i+1] = max(left[i],arr[i]);
+   
+    for(int i=n-1;i>0;i--)
+       right[i-1] = min(right[i],arr[i]);
+   
+    for(int i=1;i<n-1;i++)
+    {
+       if(left[i]<=arr[i] && right[i]>=arr[i])
+       return arr[i];
+    }
+    return -1;
+
+}
