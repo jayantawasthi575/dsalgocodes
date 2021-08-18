795e9a611459c6504e6bd7b731b8c623e7e21dce
Jayant <jayantawasthi5@gmail.com>
2021-08-18 09:54:45 +0530

    Detect Loop in linked list

---
+++ ds51.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+    public:
+    bool detectLoop(Node* head)
+    {
+        if(head==NULL || head->next==NULL)
+        return false;
+        Node* fast=head;
+        Node* slow=head;
+        while(fast->next!=NULL && fast->next->next!=NULL)
+        {
+            fast=fast->next->next;
+            slow=slow->next;
+            if(fast==slow)
+            return true;
+        }
+        return false;
+    }
+};
