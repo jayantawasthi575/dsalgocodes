655ca4b74552c5576c41c8cd0a3cd03af347589e
Jayant <jayantawasthi5@gmail.com>
2021-08-18 12:02:53 +0530

    int getNthFromLast(Node *head, int n)

---
+++ ds54.cpp
@@ -0,0 +1,20 @@
+int getNthFromLast(Node *head, int n)
+{
+     Node* fast=head;
+     Node* slow=head;
+     int i;
+     for(i=1;i<=n;i++)
+      {
+      if(fast->next==NULL && i==n)
+        return slow->data;
+      else if(fast->next==NULL)
+       return -1;
+        fast=fast->next;
+      }
+      while(fast->next!=NULL)
+      {   
+          fast=fast->next;
+          slow=slow->next;
+      }
+      return slow->next->data;
+} 
