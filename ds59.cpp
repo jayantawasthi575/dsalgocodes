59ff74f36cdba28f6cddfd7976c7363de9a39c45
Jayant <jayantawasthi5@gmail.com>
2021-08-20 12:23:01 +0530

    Find length of Loop

--- ds59.cpp
+++ ds59.cpp
@@ -24,4 +24,30 @@ int countNodesinLoop(struct Node *head)
         return c;
         c++;
     }
- 
+int countNodesinLoop(struct Node *head)
+{
+    Node* fast=head;
+    Node* slow=head;
+    int t=0;
+    while(fast->next!=NULL && fast->next->next!=NULL)
+    {
+        fast=fast->next->next;
+        slow=slow->next;
+        if(fast==slow)
+        {
+        t=1;
+        break;
+        }
+    }
+    if(t==0)
+    return 0;
+    int c=1;
+    Node* move=fast;
+    while(1)
+    {
+        move=move->next;
+        if(move==fast)
+        return c;
+        c++;
+    }
+}   
