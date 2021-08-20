9470dbf38fd462ce33ad18cb1d3d1149a6260824
Jayant <jayantawasthi5@gmail.com>
2021-08-20 18:56:28 +0530

    Check if Linked List is Palindrome

--- ds55.cpp
+++ ds55.cpp
@@ -1,20 +1,39 @@
-int getNthFromLast(Node *head, int n)
-{
-     Node* fast=head;
-     Node* slow=head;
-     int i;
-     for(i=1;i<=n;i++)
-      {
-      if(fast->next==NULL && i==n)
-        return slow->data;
-      else if(fast->next==NULL)
-       return -1;
-        fast=fast->next;
-      }
-      while(fast->next!=NULL)
-      {   
-          fast=fast->next;
-          slow=slow->next;
-      }
-      return slow->next->data;
-}
+class Solution{
+  public:
+    //Function to check whether the list is palindrome.
+    Node* reverseList(Node* head)
+    {
+        Node* pre=NULL;
+        Node* next=NULL;
+        while(head!=NULL)
+        {
+            next=head->next;
+            head->next=pre;
+            pre=head;
+            head=next;
+        }
+        return pre;
+    }
+    bool isPalindrome(Node *head)
+    {
+       if(head==NULL || head->next==NULL)
+            return true;
+       Node* slow=head;
+       Node* fast=head;
+       while(fast->next!=NULL && fast->next->next!=NULL)
+       {
+           slow=slow->next;
+           fast=fast->next->next;
+       }
+       slow->next=reverseList(slow->next);
+       slow=slow->next;
+       while(slow!=NULL)
+       {
+           if(head->data!=slow->data)
+               return false;
+           head=head->next;
+           slow=slow->next;
+       }
+       return true;
+    }
+};
