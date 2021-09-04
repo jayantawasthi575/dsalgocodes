5f21aab78ff873a51c1f67ca652713d767aa0381
Jayant <jayantawasthi5@gmail.com>
2021-09-04 07:10:09 +0530

    Minimum Depth of a Binary Tree

---
+++ ds99.cpp
@@ -0,0 +1,16 @@
+class Solution{
+  public:
+    /*You are required to complete this method*/
+    int minDepth(Node *root) {
+        if(root==NULL)
+            return 0;
+        if(root->left==NULL && root->right==NULL)
+            return 1;
+        int l=INT_MAX,r=INT_MAX;
+        if(root->left)
+            l=minDepth(root->left);
+        if(root->right)
+            r=minDepth(root->right);
+        return min(l,r)+1;
+    }
+};
