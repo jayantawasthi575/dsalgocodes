a02c4b6194df62b7487519bd3cfd0c2e95173650
Jayant <jayantawasthi5@gmail.com>
2021-11-11 18:02:00 +0530

    Minimum Cost of ropes

---
+++ ds250.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+    public:
+    //Function to return the minimum cost of connecting the ropes.
+    long long minCost(long long arr[], long long n) {
+        long long sum=0;
+        vector<long long>A;
+        for(int i=0;i<n;i++)
+        A.push_back(arr[i]);
+        sort(A.begin(),A.end());
+        if(A.size()<2)
+        {
+            return 0;
+        }
+        while(A.size()>=2)
+        {   
+            int a=A[0]+A[1];
+            sum=sum+a;
+            A.erase(A.begin());
+            A.erase(A.begin());
+            A.push_back(a);
+            sort(A.begin(),A.end());
+        }
+        return sum;
+    }
+};
