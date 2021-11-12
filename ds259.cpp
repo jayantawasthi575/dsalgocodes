2669c4326b1fbe13cac4814039c83084ac5fe897
Jayant <jayantawasthi5@gmail.com>
2021-11-12 19:37:38 +0530

    Count pairs with given sum

---
+++ ds259.cpp
@@ -0,0 +1,17 @@
+class Solution{   
+public:
+    int getPairsCount(int arr[], int n, int k) {
+        // code here
+        unordered_map<int,int> ma;
+        int cnt=0;
+        for(int i=0;i<n;i++)
+        {
+            if(ma.find(k-arr[i])!=ma.end())
+            {
+                cnt=cnt+ma[k-arr[i]];
+            }
+            ma[arr[i]]++;
+        }
+        return cnt;
+    }
+};
