dbd150e2bb62c8e7aa8fce6565fe0a9cf3632f6a
Jayant <jayantawasthi5@gmail.com>
2021-11-18 21:07:43 +0530

    Distance of nearest cell having 1

---
+++ ds291.cpp
@@ -0,0 +1,48 @@
+class Solution 
+{
+    public:
+    //Function to find distance of nearest 1 in the grid for each cell.
+	vector<vector<int>>nearest(vector<vector<int>>grid)
+	{
+	    // Code here
+	    int n=grid.size();
+	    int m=grid[0].size();
+	    vector<vector<int>> ans(n,vector<int>(m,INT_MAX));
+	    queue<pair<int,int>> q;
+	    for(int i=0;i<n;i++)
+	    for(int j=0;j<m;j++)
+	    {
+	        if(grid[i][j]==1)
+	        {
+	            ans[i][j]=0;
+	            q.push({i,j});
+	        }
+	    }
+	    while(!q.empty())
+	    {
+	        int i=q.front().first;
+	        int j=q.front().second;
+	        if((i-1)>=0 && ans[i][j]+1<ans[i-1][j]){
+	            ans[i-1][j]=ans[i][j]+1;
+	            q.push({i-1,j});
+	        }
+	        if((j-1)>=0 && ans[i][j]+1<ans[i][j-1])
+	        {
+	            ans[i][j-1]=ans[i][j]+1;
+	            q.push({i,j-1});
+	        }
+	        if((i+1)<n && ans[i][j]+1<ans[i+1][j])
+	        {
+	            ans[i+1][j]=ans[i][j]+1;
+	            q.push({i+1,j});
+	        }
+	        if((j+1)<m && ans[i][j]+1<ans[i][j+1])
+	        {
+	            ans[i][j+1]=ans[i][j]+1;
+	            q.push({i,j+1});
+	        }
+	        q.pop();
+	    }
+	    return ans;
+	}
+};
