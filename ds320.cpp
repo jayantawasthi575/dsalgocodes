9581d28b79a9564452012b5df380a9a73605d24a
Jayant <jayantawasthi5@gmail.com>
2021-12-15 03:28:12 +0530

    Generate IP Addresses

---
+++ ds320.cpp
@@ -0,0 +1,30 @@
+class Solution{
+    void recfunc(string s , int st , int e , int d , string str , 
+    vector<string> &ans){
+        if(d==3 || st>=e){
+            string temp = "";
+            if(d==3 && e-st+1 <= 3 && e-st+1>0){
+                for(int i=st ; i<=e ; i++) temp+=s[i];
+                if(stoi(temp) >= 256
+            || (temp.size()>1 && temp[0]=='0')) return;
+                ans.push_back(str+temp);
+            }
+            return;
+        }
+        string temp = "";
+        
+        for(int i=st , j=0; j<3 && i<=e ; i++ , j++){
+            temp += s[i];
+            if(stoi(temp) >= 256 
+            || (temp.size()>1 && temp[0]=='0')) return;
+            recfunc(s , i+1 , e , d+1 , str+temp+"." , ans);
+        }
+    }
+  public:
+    vector<string> genIp(string &s) {
+        vector<string> ans;
+        recfunc(s , 0 , s.size()-1 , 0 , "" , ans);
+        return ans;
+    }
+
+};
