18adbbb7e49a303ac06f30100eb18b809985f7fa
Jayant <jayantawasthi5@gmail.com>
2021-12-24 11:50:11 +0530

    Minimum number of deletions.

---
+++ ds336.cpp
@@ -0,0 +1,23 @@
+int minDeletions(string str, int n) { 
+    //complete the function here 
+    string b=str;
+    reverse(str.begin(),str.end());
+    int t[n+1][n+1];
+    for(int i=0;i<n+1;i++)
+    for(int j=0;j<n+1;j++)
+    {
+        if(i==0||j==0)
+        t[i][j]=0;
+    }
+    for(int i=1;i<n+1;i++)
+    for(int j=1;j<n+1;j++)
+    {
+        if(str[i-1]==b[j-1])
+        t[i][j]=1+t[i-1][j-1];
+        else
+        {
+            t[i][j]=max(t[i-1][j],t[i][j-1]);
+        }
+    }
+    return n-t[n][n];
+} 
