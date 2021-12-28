6065f5eef169e9c296243e9f5531c721f7da5a1d
Jayant <jayantawasthi5@gmail.com>
2021-12-29 04:19:42 +0530

    Sum of all prime numbers between 1 and N.

---
+++ ds354.cpp
@@ -0,0 +1,29 @@
+class Solution{
+	public:
+	long long int sum=0;
+	void SieveOfEratosthenes(int n)
+    {
+        bool prime[n + 1];
+        memset(prime, true, sizeof(prime));
+        for (int p = 2; p * p <= n; p++)
+        {
+            if (prime[p] == true)
+            {
+                for (int i = p * p; i <= n; i += p)
+                    prime[i] = false;
+            }
+        }
+        for(int i=2;i<=n;i++)
+        {
+            if(prime[i]==true)
+            {
+                sum=sum+i;
+            }
+        }
+    }
+   	long long int prime_Sum(int n){
+   	    // Code here
+   	    SieveOfEratosthenes(n);
+   	    return sum;
+   	}    
+};
