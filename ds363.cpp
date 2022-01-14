30939a611551db0a31370b978d1a4cfc9042fa92
Jayant <jayantawasthi5@gmail.com>
2022-01-14 09:29:39 +0530

    DFS of Graph

---
+++ ds363.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    // Function to return a list containing the DFS traversal of the graph.
+    void df(int node,vector<int>& vis,vector<int> adj[],vector<int> &dfs)
+    {
+        dfs.push_back(node);
+        vis[node]=1;
+        for(auto it:adj[node])
+        {
+            if(!vis[it])
+            {
+                df(it,vis,adj,dfs);
+            }
+        }
+    } 
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        vector<int> dfs;
+        vector<int> vis(V,0);
+        df(0,vis,adj,dfs);
+        return dfs;
+    }
+};
