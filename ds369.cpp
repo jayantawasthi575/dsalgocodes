542f3429b9d6fc1f58cc6533e24013c23861e524
Jayant <jayantawasthi5@gmail.com>
2022-01-28 11:47:42 +0530

    Topological sort

---
+++ ds369.cpp
@@ -0,0 +1,33 @@
+class Solution
+{
+	public:
+	//Function to return list containing vertices in Topological order. 
+	void findTopoSort(int node,vector<int>& vis,stack<int>& st,vector<int> adj[])
+	{
+	    vis[node]=1;
+	    for(auto it:adj[node])
+	    {
+	        if(!vis[it])
+	        findTopoSort(it,vis,st,adj);
+	    }
+	    st.push(node);
+	}
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    // code here
+	    stack<int> st;
+	    vector<int> vis(V,0);
+	    for(int i=0;i<V;i++)
+        {
+            if(vis[i]==0)
+            findTopoSort(i,vis,st,adj);
+        }
+        vector<int> topo;
+        while(!st.empty())
+        {
+            topo.push_back(st.top());
+            st.pop();
+        }
+        return topo;
+	}
+};
