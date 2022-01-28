f75a1dc3707953f31a0000dafd216e5e5bc07328
Jayant <jayantawasthi5@gmail.com>
2022-01-28 22:56:18 +0530

    Negative weight cycle

---
+++ ds372.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+	int isNegativeWeightCycle(int n, vector<vector<int>>edges){
+	    // Code here
+	    int inf=10000000;
+	    vector<int> dist(n,inf);
+	    dist[0]=0;
+	    for(int i=1;i<=n-1;i++)
+	    {
+	        for(auto it:edges)
+	        {
+	            if(dist[it[0]]+it[2]<dist[it[1]])
+	            dist[it[1]]=dist[it[0]]+it[2];
+	        }
+	    }
+	    int fl=0;
+	    for(auto it:edges)
+	    {
+	        if(dist[it[0]]+it[2]<dist[it[1]])
+	        return 1;
+	    }
+	    if(!fl)
+	    return 0;
+	}
+};
