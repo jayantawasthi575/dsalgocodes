a8cf8b4c406ee045ddb0b2d4206bbb472522c642
Jayant <jayantawasthi5@gmail.com>
2022-01-30 12:50:05 +0530

    Bipartite Graph

---
+++ ds378.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool bipartiteDfs(int node,vector<int> adj[],int color[])
+    {
+        if(color[node]==-1)
+        color[node]=1;
+        for(auto it:adj[node])
+        {
+            if(color[it]==-1){
+                color[it]=1-color[node];
+                if(!bipartiteDfs(it,adj,color))
+                {
+                    return false;
+                }
+            }
+            else if(color[it]==color[node])
+            return false;
+        }
+        return true;
+    }
+	bool isBipartite(int V, vector<int>adj[]){
+	    int color[V];
+	    memset(color,-1,sizeof color);
+	    for(int i=0;i<V;i++)
+	    {
+	        if(color[i]==-1)
+	        {
+	            if(!bipartiteDfs(i,adj,color))
+	            {
+	                return false;
+	            }
+	        }
+	    }
+	    return true;
+	}
+};
